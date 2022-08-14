36a9bd625aa1c501ddcfd6d89a24cd2a20e9f938
kalpanamaurya <kalpana.maurya1811@gmail.com>
2022-08-14 21:48:36 +0530

    index.js(now buttons is working)

--- Mini Projects/JS-Calculator/index.js
+++ Mini Projects/JS-Calculator/index.js
@@ -1,9 +1,9 @@
 let screen = document.getElementById('screen');
-button = document,getElementById('button')
+buttons = document.querySelectorAll('button')
 for(item of buttons){
           item.addEventListener('click', (e)=>{
           buttonText = e.target.innerText;
-          console.log('Button text is' , buttonTaxt)
-         
-          })
+          console.log('Button text is' , buttonText);
+           
+     })
         }
