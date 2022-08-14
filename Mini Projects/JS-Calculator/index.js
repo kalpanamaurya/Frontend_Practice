0f70e7951a2cd6d64f7de016334a079464ddeed2
kalpanamaurya <kalpana.maurya1811@gmail.com>
2022-08-14 21:21:46 +0530

    index.js(functional)

---
+++ Mini Projects/JS-Calculator/index.js
@@ -0,0 +1,9 @@
+let screen = document.getElementById('screen');
+button = document,getElementById('button')
+for(item of buttons){
+          item.addEventListener('click', (e)=>{
+          buttonText = e.target.innerText;
+          console.log('Button text is' , buttonTaxt)
+         
+          })
+        }
